7af5486b6c2ecfbab38ef3a1e3f482b9d140435f
TX2018 <mar1467659@gmail.com>
2019-11-19 09:31:02 +0000

    Added conversion

--- specs.hs
+++ specs.hs
@@ -2,6 +2,7 @@ import Test.Hspec
 import Test.QuickCheck
 import Complex
 import Fractals
+import Conversion
 
 main :: IO ()
 main = hspec $ do
@@ -49,3 +50,51 @@ main = hspec $ do
       it "return limit" $ do
         limit (Complex 0 0) (add (Complex 1 1)) 20 100 `shouldBe` Just 4
         limit (Complex 0 0) (add (Complex 0 0)) 20 100 `shouldBe` Nothing 
+
+  
+  describe "Conversion" $ do
+    describe "cell_x" $ do
+      it "calculates the width of the pixel" $ 
+        let conversion = Conversion {
+            screen_width = 10,
+            screen_height = 20,
+            origin_x = -2,
+            origin_y = 2,
+            real_width = 4,
+            real_height = 4
+        }
+        in cell_x conversion `shouldBe` 0.4
+
+
+    -- describe "real_x" $ do
+    --   it "returns the real coor of the pixel" $
+    --     do 
+    --       (real_x conversion 0 `shouldBe` (-2.0))
+    --       ((real_x conversion 9) - 1.6 < 0.001 `shouldBe` True)
+    --       ((real_x conversion 4) + 0.4 < 0.001 `shouldBe` True)
+    --       where 
+    --         conversion = Conversion {
+    --           screen_width = 10,
+    --           screen_height = 20,
+    --           origin_x = -2,
+    --           origin_y = 2,
+    --           real_width = 4,
+    --           real_height = 4 
+    --         } 
+        
+    describe "screen_to_real" $ do
+      it "returns the complex number for screen coords" $
+        do 
+          ((screen_to_real conversion (0, 0)) `shouldBe` (Complex (-2) 2))
+          where 
+            conversion = Conversion {
+              screen_width = 10,
+              screen_height = 20,
+              origin_x = -2,
+              origin_y = 2,
+              real_width = 4,
+              real_height = 4
+            }
+
+
+
